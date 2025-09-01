afc041e79e3d8f4e6d5b8c7deb8d9b74cff5b2a4
Marat Zakirov <marat61@gmail.com>
2025-09-01 17:48:04 +0300

    :A huge step towards collisions

--- simulations/particle/3dparticle.py
+++ simulations/particle/3dparticle.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import random
 import numpy as np
+import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 from typing import List
 
@@ -12,6 +13,7 @@ GRID_SIZE = 10
 GRAVITY = np.array([0, 0, -0.5])
 
 # Parameters
+# TODO add warmup iterations without collisions
 step_num = 0
 STEPS = 2100
 detailed_steps = set(range(100)) | set(range(500, 550)) | set(range(2000, 2100))
@@ -41,6 +43,27 @@ class Particles:
         Z_idx = np.logical_or(self.x[:, 2] < 0, self.x[:, 2] > NZ)
         self.v[Z_idx, 2] = -self.v[Z_idx, 2]
 
+    def find_collided_pairs(self):
+        addr = (self.x // GRID_SIZE).astype(int)
+        addr = addr[:, 0] + 100 * addr[:, 1] + 10000 * addr[:, 2]
+        addr = pd.Series(addr)
+        groups = addr.groupby(addr)
+        ij = []
+
+        # The idea is to form groups
+        # Then random shuffle each group
+        # Then truncate each group int(COLLISION_PROBABILITY * len(group))//2*2
+        # Then form one array make it reshape(-1, 2)
+        # Done process these pairs into process_collisions
+
+        for g in groups.groups:
+            idx_s = groups.get_group(g)
+            if len(idx_s) > 0:
+                ij.append(0)
+
+    def process_collisions(self):
+        pass
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.set_aspect('equal')
@@ -65,6 +88,8 @@ def update(frame):
 
     for i in range(10 - 9*int(step_num in detailed_steps)):
         gas.step()
+        gas.find_collided_pairs()
+        gas.process_collisions()
         step_num += 1
 
     if step_num > STEPS:
