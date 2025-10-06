4b6992a64d5268aaeb551d84952e1aff15ac88af
Marat Zakirov <marat61@gmail.com>
2025-10-07 00:03:15 +0300

    some corrections

--- graphs/atmos.py
+++ graphs/atmos.py
@@ -2,19 +2,16 @@ import ussa1976
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Нужно пересчитать для случая температуры как в данных ussa1976
-
-def transform_v(x, p=0.8, n=14, o=16):
-    #return ((o/n)*(1-p) + p)*x
-    return 1.1*x
-
 def standart_bolzman_formula_for_nitrogen(altitude, max_rho: float) -> np.ndarray:
-    #return max_rho*np.exp(-1.1e-4 *altitude)
     return max_rho * np.exp(-0.0001147 * altitude)
 
-def neo_bolzman_formula_for_nitrogen(altitude, max_rho: float, T_0: float = 288) -> np.ndarray:
-    val = 0.0033 * (T_0-transform_v(0.00943)*altitude)**(5/2)
-    return max_rho * val/val.max()
+def neo_bolzman_formula_for_nitrogen(altitude, max_rho: float, T_0: float = 288.15) -> np.ndarray:
+    if 1:
+        val = (T_0-0.00943*altitude)**(5/2)
+        return max_rho * val/val.max()
+    else:
+        val = (1-0.0065*altitude/288.15)*(4.09)
+        return max_rho * val / val.max()
 
 # Create a regular altitude mesh from 0 to 10000 meters
 altitudes = np.arange(0.0, 10001.0, 1.0)
@@ -26,13 +23,13 @@ ds = ussa1976.compute(z=altitudes, variables=["rho"])
 plt.figure(dpi=100)
 
 # Plot the air density ('rho') against altitude ('z')
-plt.plot(altitudes, ds.rho)
+plt.plot(altitudes, ds.rho, c='r')
 
 bolzman_nitro_rho = standart_bolzman_formula_for_nitrogen(altitudes, max_rho=ds.rho.max().item())
 neo_nitro_rho = neo_bolzman_formula_for_nitrogen(altitudes, max_rho=ds.rho.max().item())
 
-plt.plot(altitudes, bolzman_nitro_rho)
-plt.plot(altitudes, neo_nitro_rho)
+plt.plot(altitudes, bolzman_nitro_rho, c='b')
+plt.plot(altitudes, neo_nitro_rho, c='g')
 
 # Add grid for better readability
 plt.grid(True)
